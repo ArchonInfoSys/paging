99b654c0f201a866656615a6681146d543abfd49
Chad Lee <chad.lee@archoninfosys.com>
2016-05-13 14:39:35 -0500

    Use GitVersion for versioning
    +semver:feature

--- AssemblyInfoCommon.cs
+++ AssemblyInfoCommon.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Archon Paging Utilities")]
 [assembly: AssemblyCompany("Archon Information Systems, LLC")]
-[assembly: AssemblyCopyright("Copyright © 2008-2014 Archon Information Systems, LLC")]
+[assembly: AssemblyCopyright("Copyright © 2008-2016 Archon Information Systems, LLC")]
 [assembly: ComVisible(false)]
